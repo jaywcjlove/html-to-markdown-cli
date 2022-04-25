226cd10ef531096b88bc3aa90a48b8fda2f3e974
jaywcjlove <398188662@qq.com>
2022-04-25 19:15:46 +0800

    feat: add remarkPlugins/rehypePlugins options.

--- packages/html-to-markdown/src/index.ts
+++ packages/html-to-markdown/src/index.ts
@@ -1,4 +1,4 @@
-import { unified } from 'unified';
+import { unified, PluggableList } from 'unified';
 import rehypeParse, { Options as RehypeParseOptions} from 'rehype-parse';
 import rehypeRemark from 'rehype-remark';
 import remarkStringify from 'remark-stringify';
@@ -11,17 +11,23 @@ type Options = {
   html?: string
   url?: string
   rehypeParseOption?: RehypeParseOptions;
+  /** List of [remark plugins](https://github.com/remarkjs/remark/blob/main/doc/plugins.md#list-of-plugins) to use. See the next section for examples on how to pass options */
+  remarkPlugins?: PluggableList;
+  /** List of [rehype plugins](https://github.com/rehypejs/rehype/blob/main/doc/plugins.md#list-of-plugins) to use. See the next section for examples on how to pass options */
+  rehypePlugins?: PluggableList;
 }
 
 export default async function htmlToMarkdown(options: Options = {}) {
-  const { rehypeParseOption } = options;
+  const { rehypeParseOption, remarkPlugins = [], rehypePlugins = [] } = options;
   const file = await unified()
     .use(rehypeParse, { fragment: true, ...rehypeParseOption })
     .use(rehypeRemark)
+    .use(rehypeIgnore)
     .use(remarkGfm)
     .use(rehypeVideo)
-    .use(rehypeIgnore)
     .use(rehypeFormat)
+    .use(remarkPlugins || [])
+    .use(rehypePlugins || [])
     .use(remarkStringify)
     .processSync(options.html)
   return String(file);
